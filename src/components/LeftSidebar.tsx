b7afec40cc3b2b1d21a229b4bef51086b45688b5
gpeddle <gpeddle@gmail.com>
2024-03-04 16:32:25 -0500

    add bullets and vertical line

--- src/components/LeftSidebar.tsx
+++ src/components/LeftSidebar.tsx
@@ -5,6 +5,8 @@ import Drawer from '@mui/material/Drawer';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
 import { useTheme } from '@mui/material/styles';
 import { useFormDefinition } from '../contexts/FormDefinitionContext';
 
@@ -24,21 +26,43 @@ const LeftSidebar: React.FC<{ setCurrentStep: (step: number) => void; currentSte
             sx={{
                 width: drawerWidth,
                 flexShrink: 0,
-                [`& .MuiDrawer-paper`]: { 
-                    width: drawerWidth, 
+                [`& .MuiDrawer-paper`]: {
+                    width: drawerWidth,
                     boxSizing: 'border-box',
-                    marginTop: '64px',  // Assuming the AppBar height is 64px. Adjust if your AppBar height is different
+                    marginTop: '64px',
+                    position: 'relative',
                 },
             }}
         >
             <List>
-                {formDefinition?.map((step) => (
-                    <ListItem 
-                      button 
-                      key={step.step} 
+                {formDefinition?.map((step, index) => (
+                    <ListItem
+                      button
+                      key={step.step}
                       onClick={() => handleListItemClick(step.step)}
                       selected={currentStep === step.step}
+                      sx={{
+                          position: 'relative',
+                          '&::before': {
+                              content: '""',
+                              position: 'absolute',
+                              left: '25px', // Adjusted based on your feedback
+                              width: '2px',
+                              backgroundColor: 'gray',
+                              top: index === 0 ? '50%' : '0',
+                              bottom: index === formDefinition.length - 1 ? '50%' : '0',
+                              zIndex: 0, // Ensure the line is behind the icons
+                          }
+                      }}
                     >
+                        <ListItemIcon sx={{ zIndex: 1 }}>
+                            <FiberManualRecordIcon 
+                                fontSize="small" 
+                                sx={{ 
+                                    color: currentStep === step.step ? theme.palette.primary.main : 'inherit'
+                                }} 
+                            />
+                        </ListItemIcon>
                         <ListItemText primary={step.stepName} />
                     </ListItem>
                 ))}
